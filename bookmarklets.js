04a159ad4a9ae1f3e043e960296939bc5d243319
kidpixo <kidpixo@gmail.com>
2015-12-02 13:14:43 +0100

    Create bookmarklets.js
    Useful javascript bookmarklets, first version with gmail to geekcookiespodcast@gmail.com and buffer.app

---
+++ bookmarklets.js
@@ -0,0 +1,33 @@
+////////////////////////////////////////////////////////////////////////////////////
+//
+// send the current page to geekcookiespodcast@gmail.com :
+//
+// Title = Page Title
+// Body  = URL 
+
+// Beatuful version
+javascript: var t = encodeURI(document.title);
+var u = encodeURI(window.location);
+window.open('https://mail.google.com/mail/?view=cm&to=geekcookiespodcast%40gmail.com&fs=1&tf=1&su=' + t + '&body=' + u);
+
+// bookmarlet
+javascript:var t=encodeURI(document.title);var u=encodeURI(window.location);window.open('https://mail.google.com/mail/?view=cm&to=geekcookiespodcast%40gmail.com&fs=1&tf=1&su='+t+'&body='+u);
+
+////////////////////////////////////////////////////////////////////////////////////
+//
+// send the current page to the private bufferapp address, see 
+// https://buffer.com/guides/email for yours and command description.
+// Put the email where "XXXYOUR EMAIL HEREXXX" is in the bookmarlet.
+//
+// Title = Page Title
+// Body  = @link URL 
+
+// Beatuful version
+javascript: var t = encodeURI(document.title);
+var u = '@l ' + encodeURI(window.location);
+window.open('https://mail.google.com/mail/?view=cm&to=XXXYOUR EMAIL HEREXXX&fs=1&tf=1&su=' + t + '&body=' + u);
+
+// bookmarlet
+javascript:var t=encodeURI(document.title);var u='@l '+encodeURI(window.location);window.open('https://mail.google.com/mail/?view=cm&to=XXXYOUR EMAIL HEREXXX&fs=1&tf=1&su='+t+'&body='+u);
+
+////////////////////////////////////////////////////////////////////////////////////
